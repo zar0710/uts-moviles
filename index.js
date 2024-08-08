ff1210bf897669d235da485f64d591de9e3ed468
zar0710 <ninozarela@gmail.com>
2024-08-07 19:56:38 -0500

    Primer Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let numeroUno = parseFloat(prompt("Escriba el primer numero"))
+let numeroDos = parseFloat(prompt("Escriba el segundo numero"))
+
+if(numeroUno > numeroDos){
+    console.log("El numero mayor es: ",numeroUno);
+}
+else{
+    console.log("El numero mayor es: ",numeroDos);
+}
